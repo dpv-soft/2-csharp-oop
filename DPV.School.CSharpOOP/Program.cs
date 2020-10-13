aa880a11c548f913d6391e63bb6814c052f02181
Trufanov Roman <pingvin1308@gmail.com>
2020-10-13 10:14:21 +0300

    init

---
+++ DPV.School.CSharpOOP/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DPV.School.CSharpOOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
